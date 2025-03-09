3f93616aec1f11bef440704e342bc833f0dc2b83
Alexis Giles <alexisgiles00@gmail.com>
2025-03-09 13:57:05 -0600

    Add files via upload

---
+++ practica2_1/Token.java
@@ -0,0 +1,47 @@
+package practica2_1;
+public class Token {
+
+    final TipoToken tipo;
+    final String lexema;
+    final Object literal;
+    final int line;
+    public Object type;
+
+    public Token(TipoToken tipo, String lexema, Object literal, int line) {
+        this.tipo = tipo;
+        this.lexema = lexema;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public Token(TipoToken tipo, String lexema, int line) {
+        this(tipo, lexema, null, line);
+    }
+
+    public TipoToken getTipo() {
+        return tipo;
+    }
+
+    public String getLexema() {
+        return lexema;
+    }
+
+    public Object getLiteral() {
+        return literal;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public String toString() {
+        if (literal != null) {
+            return "<" + tipo + ", lexema: " + lexema + ", literal: " + literal + ", línea: " + line + ">";
+        } else if (tipo == TipoToken.EOF){
+            return "<" + tipo + ", lexema: $" + ">";
+        }
+        else {
+            return "<" + tipo + ", linea: "  + line + ">";
+        }
+    }
+}
